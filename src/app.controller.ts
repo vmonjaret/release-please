b8bcf8f7bdbadc7a4241f8691ddb54d2eb93a31a
Vincent Monjaret <9928313+vmonjaret@users.noreply.github.com>
2023-05-23 11:57:11 +0200

    fix(hello): forgot name fromm controller (#1)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Bind, Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Bind(Param())
+  getHello(name?: string): string {
+    return this.appService.getHello(name);
   }
 }
